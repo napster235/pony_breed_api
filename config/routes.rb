af8d5aa72f4e2d58efbe8154bfb7d9d0e928e47d
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-10-12 15:51:47 +0300

    Add HomePage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home_page#index'
   get 'api/v1/random_pony' => 'api/v1/pony_breeds#random_pony', as: :random_pony
   get 'api/v1/pony_by_name' => 'api/v1/pony_breeds#pony_by_name', as: :pony_by_name
   get 'api/v1/pony_by_key' => 'api/v1/pony_breeds#pony_by_key', as: :pony_by_key
